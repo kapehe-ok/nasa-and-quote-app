a0ca462d7d148d638fae1cd7bf5d5a55e617ccc5
Kapehe <sarahkapehe@gmail.com>
2020-05-01 16:12:07 -0700

    updating to Link

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
  
   return (
     <div className="home">
-      <NavLink className="home-link" to="/nasaphoto" exact >See into the stars!</NavLink>
+      <Link className="home-link" to="/nasaphoto" exact >See into the stars!</Link>
     </div>
   );
 }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <div className="navbar">
             <ul>
-                <NavLink className="link" to="/" >Take Me Home</NavLink>
+                <Link className="link" to="/" >Take Me Home</Link>
             </ul>
         </div>
     )
